bf99ada7b63899e1a268c740653a976814ad24d3
Richard Lund <richard.lund@crownpeak.com>
2020-10-01 10:11:51 +0100

    Issue #43, add support for uploading and relinking images (css/js/etc) used within pages and wrappers.

--- src/crownpeak/utils.js
+++ src/crownpeak/utils.js
@@ -1,6 +1,12 @@
 const fs = require("fs");
 const path = require('path');
 
+const reAssets = [
+    /<(img|script|input)\s+(?:[^>]*?)src=((["']?)(.*?)\3)/ig,
+    /<(link)\s+(?:[^>]*?)href=((["']?)(.*?)\3)/ig,
+    /(url)\s*\(\s*((["']?)([^)]*)\3)\s*\)/ig
+];
+
 const colouriseErrors = () => {
     // See https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
     const originalError = console.error;
@@ -27,9 +33,11 @@ const getPaths = (origin, url) => {
         filepath = path.join(root, url.substr(1));
     else
         filepath = path.resolve(path.dirname(origin), url);
+    let folder = path.dirname(filepath).substr(root.length + 1).replace(/\\/g, "/") + "/";
+    if (folder === "/") folder = "";
     return {
         path: filepath,
-        folder: path.dirname(filepath).substr(root.length + 1).replace(/\\/g, "/") + "/",
+        folder: folder,
         filename: filename
     };
 };
@@ -55,6 +63,42 @@ const getRecursive = function(dir, extn) {
     return results;
 }
 
+const replaceAssets = (file, content, cssParser) => {
+    let result = content;
+    let uploads = [];
+
+    var matches;
+    for (var i = 0, len = reAssets.length; i < len; i++) {
+        while (matches = reAssets[i].exec(result)) {
+            if (matches && matches.length > 4) {
+                let url = matches[4];
+                if (url && url.indexOf("http") < 0 && url.indexOf("//") < 0) {
+                    //console.log(`Found candidate ${url}`);
+                    const { path: filepath, folder: dir, filename } = getPaths(file, url);
+                    if (fs.existsSync(filepath)) {
+                        let replacement = `"<%= Asset.Load(Asset.GetSiteRoot(asset).AssetPath + \"/${dir}${filename}\").GetLink() %>"`;
+                        //console.log(`Replacement is ${replacement}`);
+                        result = result.replace(matches[2], replacement);
+                        if (matches[1] === "link" && fs.lstatSync(filepath).isFile()) {
+                            // If this is CSS, it needs to be parsed separately
+                            const result = cssParser.parse(filepath, fs.readFileSync(filepath, "utf8"), "");
+                            if (result.content && result.uploads && result.uploads.length) {
+                                uploads.push({source: filepath, name: filename, destination: dir, content: result.content});
+                                uploads = uploads.concat(result.uploads);
+                            } else {
+                                uploads.push({source: filepath, name: filename, destination: dir});
+                            }
+                        } else {
+                            uploads.push({source: filepath, name: filename, destination: dir});
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return { content: result, uploads: uploads };
+};
+
 const getRoot = function() {
     return process.cwd();
 };
@@ -63,5 +107,6 @@ module.exports = {
     colouriseErrors: colouriseErrors,
     colorizeErrors: colouriseErrors,
     getPaths: getPaths,
-    getRecursive: getRecursive
+    getRecursive: getRecursive,
+    replaceAssets: replaceAssets
 };
