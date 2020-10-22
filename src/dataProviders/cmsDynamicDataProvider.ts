83d02b18a22c959be7bd2c843cf4be42f5ed0c6a
Richard Lund <richard.lund@crownpeak.com>
2020-10-22 14:42:22 +0100

    Issue #35, replace synchronous XMLHttpRequest with asynchronous fetch.

--- src/dataProviders/cmsDynamicDataProvider.ts
+++ src/dataProviders/cmsDynamicDataProvider.ts
@@ -2,19 +2,12 @@ import CmsDataCache from '../common/cmsDataCache';
 import { ICmsDataProvider } from './ICmsDataProvider';
 
 export default class CmsDynamicDataProvider implements ICmsDataProvider {
-  private _getData(query: string) {
-    const request = new XMLHttpRequest(); // TODO: Replace with synchronous implementation of fetch, as XMLHttpRequest is deprecated.
-    request.open('GET', CmsDataCache.cmsDynamicDataLocation + query, false);
-    request.send(null);
-
-    if (request.status === 200) {
-      return JSON.parse(request.responseText);
-    }
+  private async _getData(query: string) {
+    return await (await fetch(CmsDataCache.cmsDynamicDataLocation + query)).json();
   }
 
-  getSingleAsset(assetId: number) {
-    const data = this._getData('&fl=id,custom_t_json:%5Bjson%5D&q=id:' + assetId);
-
+  async getSingleAsset(assetId: number) {
+    const data = await this._getData('&fl=id,custom_t_json:%5Bjson%5D&q=id:' + assetId);
     CmsDataCache.set(assetId,
       data && data.response && data.response.docs && data.response.docs.length > 0
         ? data.response.docs[0].custom_t_json
@@ -22,7 +15,7 @@ export default class CmsDynamicDataProvider implements ICmsDataProvider {
     );
   }
 
-  getDynamicQuery(query: string) {
-    return this._getData('&' + query);
+  async getDynamicQuery(query: string) {
+    return await this._getData('&' + query);
   }
 }
--- src/dataProviders/cmsStaticDataProvider.ts
+++ src/dataProviders/cmsStaticDataProvider.ts
@@ -2,23 +2,16 @@ import CmsDataCache from '../common/cmsDataCache';
 import { ICmsDataProvider } from './ICmsDataProvider';
 
 export default class CmsStaticDataProvider implements ICmsDataProvider {
-  private _getData(filename: string) {
-    const request = new XMLHttpRequest(); // TODO: Replace with synchronous implementation of fetch, as XMLHttpRequest is deprecated.
-    request.open('GET', CmsDataCache.cmsStaticDataLocation + '/' + filename, false);
-    request.send(null);
-
-    if (request.status === 200) {
-      return JSON.parse(request.responseText);
-    }
+  private async _getData(filename: string) {
+    return await (await fetch(CmsDataCache.cmsStaticDataLocation + '/' + filename)).json();
   }
 
-  getSingleAsset(assetId: number) {
-    const data = this._getData(assetId + '.json');
-
+  async getSingleAsset(assetId: number) {
+    const data = await this._getData(assetId + '.json');
     CmsDataCache.set(assetId, data || {});
   }
 
-  getCustomData(filename: string) {
-    return this._getData(filename);
+  async getCustomData(filename: string) {
+    return await this._getData(filename);
   }
 }
