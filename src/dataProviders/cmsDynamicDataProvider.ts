e08890e36094dc1f0f1fed8e6d75fd3e176f1c29
Richard Lund <richard.lund@crownpeak.com>
2020-11-03 09:25:37 +0000

    Restore synchronous option for data load.

--- src/dataProviders/cmsDynamicDataProvider.ts
+++ src/dataProviders/cmsDynamicDataProvider.ts
@@ -6,6 +6,16 @@ export default class CmsDynamicDataProvider implements ICmsDataProvider {
     return await (await fetch(CmsDataCache.cmsDynamicDataLocation + query)).json();
   }
 
+  private _getDataSync(query: string) {
+    const request = new XMLHttpRequest();
+    request.open('GET', CmsDataCache.cmsDynamicDataLocation + query, false);
+    request.send(null);
+
+    if (request.status === 200) {
+      return JSON.parse(request.responseText);
+    }
+  }
+
   async getSingleAsset(assetId: number) {
     const data = await this._getData('&fl=id,custom_t_json:%5Bjson%5D&q=id:' + assetId);
     CmsDataCache.set(assetId,
@@ -15,7 +25,20 @@ export default class CmsDynamicDataProvider implements ICmsDataProvider {
     );
   }
 
+  getSingleAssetSync(assetId: number) {
+    const data = this._getDataSync('&fl=id,custom_t_json:%5Bjson%5D&q=id:' + assetId);
+    CmsDataCache.set(assetId,
+      data && data.response && data.response.docs && data.response.docs.length > 0
+        ? data.response.docs[0].custom_t_json
+        : {}
+    );
+  }
+
   async getDynamicQuery(query: string) {
     return await this._getData('&' + query);
+  }
+
+  getDynamicQuerySync(query: string) {
+    return this._getDataSync('&' + query);
   }
 }
--- src/dataProviders/cmsStaticDataProvider.ts
+++ src/dataProviders/cmsStaticDataProvider.ts
@@ -6,12 +6,31 @@ export default class CmsStaticDataProvider implements ICmsDataProvider {
     return await (await fetch(CmsDataCache.cmsStaticDataLocation + '/' + filename)).json();
   }
 
+  private _getDataSync(filename: string) {
+    const request = new XMLHttpRequest();
+    request.open('GET', CmsDataCache.cmsStaticDataLocation + '/' + filename, false);
+    request.send(null);
+
+    if (request.status === 200) {
+      return JSON.parse(request.responseText);
+    }
+  }
+
   async getSingleAsset(assetId: number) {
     const data = await this._getData(assetId + '.json');
     CmsDataCache.set(assetId, data || {});
   }
 
+  getSingleAssetSync(assetId: number) {
+    const data = this._getDataSync(assetId + '.json');
+    CmsDataCache.set(assetId, data || {});
+  }
+
   async getCustomData(filename: string) {
     return await this._getData(filename);
+  }
+
+  getCustomDataSync(filename: string) {
+    return this._getDataSync(filename);
   }
 }
