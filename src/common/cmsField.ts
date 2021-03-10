6ac4718e08c07200241e23d78b6989d4ba6564a4
Richard Lund <richard.lund@crownpeak.com>
2021-03-10 14:40:11 +0000

    Add extraction of WCO script snippets from content in CmsField.

--- src/common/cmsField.ts
+++ src/common/cmsField.ts
@@ -38,17 +38,46 @@ export default class CmsField extends String {
   data(): any {
     if (typeof(this.value) !== "undefined" 
       && this.value !== null 
-      && !(typeof(this.value) === "number" && isNaN(this.value)))
-      return this.value;
+      && !(typeof(this.value) === "number" && isNaN(this.value))) {
+      return this.parseValue(this.value);
+    }
 
     const dataSource = CmsDataCache.dataSource;
     if (dataSource) {
       if (Array.isArray(dataSource)) {
         const index = dataSource.index;
-        if (dataSource[index]) return dataSource[index][this.cmsFieldName];
+        if (dataSource[index]) {
+          return this.parseValue(dataSource[index][this.cmsFieldName]);
+        }
       }
-      if (dataSource) return dataSource[this.cmsFieldName];
+      if (dataSource) {
+        return this.parseValue(dataSource[this.cmsFieldName]);
+      }
     }
     return this.cmsFieldName;
+  }
+
+  private hashCode(s: string): number {
+    return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
+  }
+
+  private parseValue(value: string): string {
+    if (!value) return value;
+    const regex = /<script\s[^>]*src="([^"]+?)"/ig;
+    let result;
+    while ((result = regex.exec(value)) !== null) {
+      const url = result[1];
+      if (url.indexOf("snippet.omm.crownpeak.com") >= 0) {
+        const id = "script-" + this.hashCode(url);
+        if (!document.getElementById("id")) {
+          let scr = document.createElement("script");
+          scr.id = id;
+          scr.async = true;
+          scr.src = url;
+          document.body.appendChild(scr);
+        }
+      }
+    }
+    return value;
   }
 }
